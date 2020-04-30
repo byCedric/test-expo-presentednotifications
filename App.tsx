59863a3829e235abc5f82a13f17bf5b4ddba9a5d
Cedric van Putten <me@bycedric.com>
2020-04-30 22:44:39 +0200

    write a small demo with presented notifications

--- App.tsx
+++ App.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  const [presented, setPresented] = useState<number>();
+
+  const onCheck = useCallback(async () => {
+    const result = await Notifications.getPresentedNotificationsAsync();
+    console.log({ result });
+    setPresented(result.length);
+  }, []);
+
+  const onSend = useCallback(() => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Look at that notification',
+        body: "I'm so proud of myself!",
+      },
+      trigger: null,
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.spacer}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <Button title='Send notification' onPress={onSend} />
+      </View>
+      <View style={styles.spacer}>
+        <Text>{presented === undefined ? '?' : presented} notifications presented</Text>
+        <Button title='Check notifications' onPress={onCheck} />
+      </View>
     </View>
   );
 }
@@ -15,5 +49,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  spacer: {
+    margin: 16,
   },
 });
